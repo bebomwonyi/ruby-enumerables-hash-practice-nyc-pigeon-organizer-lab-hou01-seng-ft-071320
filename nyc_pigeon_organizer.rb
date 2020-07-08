e2736e4af71f37d4cbdaa87bc04a4d9e02df9b17
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-08 23:36:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   hash.each_with_object({}) do |(k,v),a|
-    v.each do |k,v|
-      v.each do |n|
-        if !a[n]
-          a[n]={}
+    v.each do |kk,nn|
+      v.each do |nn|
+        if !a[nn]
+          a[nn]={}
       end
-      if !a[n][k]
-        !a[n][k]=[]
+      if !a[nn][k]
+        !a[nn][k]=[]
       end
-      a[n][k].push()
+      a[nn][k].push(kk)
     end
     a
   end
