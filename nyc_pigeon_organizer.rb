7bc9f87738d6172e00a0ed7ee60e02fa9e85f861
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-08 23:45:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  answer = data.each_with_object({}) do |(k,v),a|
+    v.each do |kk,nn|
+      nn.each do |nn|
+        if !a[nn]
+          a[nn]={}
+        end
+        if !a[nn][k]
+          !a[nn][k]=[]
+        end
+        a[nn][k].push(kk.to_s)
+      end
+    end
+  end
 end
